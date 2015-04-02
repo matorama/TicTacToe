d742b7951dfba6d32f8ff772cfdb3a4f98ea5df5
Mathieu Fallows <mathieufallows@gmail.com>
2015-04-02 16:57:33 -0700

    made x and o's alternate

--- js/TicTacToeController.js
+++ js/TicTacToeController.js
@@ -23,50 +23,36 @@ angular
         
 
         self.counter; //keeps count of the successful clicks
-        self.currentXPosition; 
         self.moveXHere = moveXHere; 
+        self.moveOHere = moveOHere;
+        self.playerTurn = playerTurn;
 
-        function moveXHere($index){
-            console.log("clicked a box")
-            self.counter++
-            self.holes[$index].xIsHere = true;
-            // var index = self.holes.indexOf(hole);
-            // console.log(index)
-            // self.holes[index].xIsHere = true;
-            // self.currentMolePosition = index;
-            // self.holes[index];
-
-
+        function playerTurn($index){
+            self.counter++;
+            if (self.counter % 2 === 0 && ){
+                moveXHere($index);
+            }
+            else {
+                moveOHere($index);
+            }
         }
 
 
-        // //called using ng-click directive in the view
-        // function whackMole($index){
-        //     //only execute the following code if the hole
-        //     //that was clicked has a mole in it
-        //     if($index === self.currentMolePosition){
+        function moveXHere($index){
+            console.log("clicked a box");
+            self.holes[$index].xIsHere = true;
+        }
 
-        //         //generate a random number to move the mole to a new hole
-        //         var randomIndex = Math.floor(Math.random() * self.holes.length);
-               
-        //         // this while loop ensures that the next hole the mole 
-        //         // moves to will not be the same as the current hole
-        //         while(randomIndex === self.currentMolePosition){
-        //             randomIndex = Math.floor(Math.random() * self.holes.length);
-        //         }
 
-        //         // self.holes[$index].xIsHere = false;
-        //         self.holes[randomIndex].xIsHere = true;
-        //         // self.currentMolePosition = randomIndex;
-        //         self.counter++;
-        //     }
-        // }
+        function moveOHere($index){
+            console.log("clicked a box");
+            self.holes[$index].oIsHere = true;
+        }
 
-        //this function is wrapped in an IIFE (immediately-invoked function expression)
-        //it runs as soon as this file loads to initialize the game
+
+ 
+
         (function init(){
-            self.currentXPosition = 0;
-            // whackMole(0);
             self.counter = 0;
         })();
 
